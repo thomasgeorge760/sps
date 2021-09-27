6a2e43c3e5cdaf97ddf284e4fd346898817f6110
thomasgeorge760 <thomasgeorge760@gmail.com>
2021-09-27 10:25:29 +0530

    test

--- java.js
+++ java.js
@@ -20,3 +20,20 @@ function resize3(){
     document.getElementById('iframe3').src+='?autoplay=1';
     
 }
+
+function resize4(){
+    document.getElementById('image4').style.display='none';
+    var a=document.getElementById('video4');
+    a.style.display='block';
+    document.getElementById('iframe4').src+='?autoplay=1';
+    
+}
+
+function resize5(){
+    document.getElementById('image5').style.display='none';
+    document.getElementById('iframe5').src+='?autoplay=1';
+    var a=document.getElementById('video5');
+    a.style.display='block';
+    
+    
+}
